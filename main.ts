3a465816fcff7b929236c19e81b4ab55c6147681
TJY228 <113530991+TJY228@users.noreply.github.com>
2022-09-14 21:56:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,71 @@
+basic.showString("Hello!")
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        # . # . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # # . .
+        # . . . .
+        # . # # .
+        # # # # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.showString("I’m 12 Yrs Old")
+    basic.pause(2000)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
